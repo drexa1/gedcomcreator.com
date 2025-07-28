44bc38f5b7acf24c495a211967cef2330c71f4ac
Diego Ruiz Barbero <drexa1@hotmail.com>
2025-07-28 19:20:46 +0200

    gh-pages

--- src/menu/javascript-natural-sort.d.ts
+++
@@ -1,4 +0,0 @@
-// Data type definitions for the javascript-natural-sort library.
-declare module 'javascript-natural-sort' {
-  export default function naturalSort(a: string, b: string): number;
-}
--- src/model/language.ts
+++
@@ -1,6 +0,0 @@
-export interface Language {
-    id: string;
-    name: string;
-    iso?: string | null;
-    abbreviation?: string;
-}
