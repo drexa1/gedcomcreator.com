ba4754c689f321d5f809343c14e9998be0bfed0a
Diego Ruiz <diego@podproza.cz>
2025-08-18 16:39:20 +0200

    Relocate contact form reasons

--- src/contact.tsx
+++ src/contact.tsx
@@ -174,9 +174,11 @@ export function ContactForm({ open, onClose, onI18nLanguage }: ContactFormProps)
                 <FormattedMessage id="contact.we.speak" defaultMessage="We speak" />&nbsp;
                 {i18nLanguages.map((lang, idx) => (
                     <span key={lang}>
-                        <a href="#" onClick={(e) => {e.preventDefault(); onI18nLanguage(lang)}} /* className={lang === intl.locale ? "selected-language" : ""} */ >
-                            <FormattedMessage id={`i18n.language.${lang}`} defaultMessage={lang} />
-                        </a>{idx < i18nLanguages.length - 1 ? ", " : ""}
+                        <a
+                            // className={lang === intl.locale ? "selected-language" : ""}
+                            // eslint-disable-next-line jsx-a11y/anchor-is-valid
+                            onClick={() => onI18nLanguage(lang)}
+                        ><FormattedMessage id={`i18n.language.${lang}`} defaultMessage={lang}/></a>{idx < i18nLanguages.length - 1 ? ", " : ""}
                     </span>
                 ))}
             </div>
