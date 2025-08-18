f1abb2f20cb1789987862c369da440de51b1845d
Diego Ruiz <diego@podproza.cz>
2025-08-18 17:15:26 +0200

    Relocate contact form reasons

--- src/chart.tsx
+++ src/chart.tsx
@@ -244,11 +244,11 @@ export function Chart(props: ChartProps) {
                         <div id="legend" className="legend">
                             <svg>
                                 <rect x="10" y="10" width="275" height="60" stroke="black" strokeDasharray="5,5" fill="none" strokeWidth="2"/>
-                                <text x={275} y={35} fontSize="16" fill="black" textAnchor="middle">
-                                    <tspan x={275 / 2} dy="0">
+                                <text x={295} y={35} fontSize="16" fill="black" textAnchor="middle">
+                                    <tspan x={295 / 2} dy="0">
                                         <FormattedMessage id="legend.stroke.1" defaultMessage="Individuals with a dashed line"/>
                                     </tspan>
-                                    <tspan x={275 / 2} dy="20">
+                                    <tspan x={295 / 2} dy="20">
                                         <FormattedMessage id="legend.stroke.2" defaultMessage="have more relatives"/>
                                     </tspan>
                                 </text>
